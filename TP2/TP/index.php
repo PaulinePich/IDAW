cae499864460d1f8fceb3060d31a33996310e13e
PaulinePich <62943136+PaulinePich@users.noreply.github.com>
2021-03-15 09:35:22 +0100

    TP2

---
+++ TP2/TP/index.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<html>
+  <head>
+    <title>TP2</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>L'heure</h1>
+    <?php
+    ?>
+    <p>Voici l'heure</p>
+  </body>
+</html>
