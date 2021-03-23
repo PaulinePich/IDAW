4ce489b54db1e75bfcc2a4e7b7c8a00218329208
PaulinePich <62943136+PaulinePich@users.noreply.github.com>
2021-03-23 16:27:02 +0100

    !

--- TP3/partie2/connected.php
+++ TP3/partie2/connected.php
@@ -1,48 +1,56 @@
 <?php
-        $login = "anonymous";
 
-        //database connexion
+$login = "anonymous";
+
             $servername = 'localhost';
+            $dbname = 'pauline';
             $username = 'root';
             $password = 'root';
-            $database='pauline';
             
-            //On établit la connexion
-            $conn = new mysqli($servername, $username, $password,$database);
-            
-            //On vérifie la connexion
-            if($conn->connect_error){
-                die('Erreur : ' .$conn->connect_error);
-            }
+            try{
+                $dbco = new PDO("mysql:host=$servname;dbname=$dbname", $user, $pass);
+                $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
+                $sql = "SELECT * from login";
+                $dbco->exec($sql);
 
-            $sql = $conn->query("SELECT * from login");
-        
-        $errorText = "";
-        $successfullyLogged = false;
-        if(isset($_POST['login']) && isset($_POST['password'])) {
-        $tryLogin=$_POST['login'];
-        $tryPwd=$_POST['password'];
-        
 
-        // si login existe et password correspond
-            while($row = $sql->fetch_assoc()){ 
-                if($row['Login']==$tryLogin && $row['Password']==$tryPwd){
-                $successfullyLogged = true;
-                $login = $tryLogin;
+                $errorText = "";
+                $successfullyLogged = false;
+                if(isset($_POST['login']) && isset($_POST['password'])) {
+                $tryLogin=$_POST['login'];
+                $tryPwd=$_POST['password'];
+                
+        
+                // si login existe et password correspond
+                    while($row = $sql->fetch_assoc()){ 
+                        if($row['Login']==$tryLogin && $row['Password']==$tryPwd){
+                        $successfullyLogged = true;
+                        $login = $tryLogin;
+                        } 
+                        else{
+                        $errorText = "Erreur de login/password veuillez réessayer<br>";
+                        }
+                    }
                 } 
-                else{
-                $errorText = "Erreur de login/password veuillez réessayer<br>";
+                else
+                $errorText = "Merci d'utiliser le formulaire de Connection<br>";
+                
+                if(!$successfullyLogged) {
+                echo $errorText;
+                } else {
+                //echo "<h1>Bienvenu ".$login."</h1>";
+                $_SESSION['login'] = $login;
                 }
+                
             }
-        } 
-        else
-        $errorText = "Merci d'utiliser le formulaire de Connection<br>";
+            
+            catch(PDOException $e){
+              echo "Erreur : " . $e->getMessage();
+            }
+
+
         
-        if(!$successfullyLogged) {
-        echo $errorText;
-        } else {
-        //echo "<h1>Bienvenu ".$login."</h1>";
-        $_SESSION['login'] = $login;
-        }
+       
        
     ?>
