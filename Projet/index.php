beb3198169cfc063d86717d526527b1fcf140017
PaulinePich <62943136+PaulinePich@users.noreply.github.com>
2021-03-26 15:49:03 +0100

    début projet

---
+++ Projet/index.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+      <div class="header">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <title>Site de Nassim Bouaziz & Pauline Pichon </title>
+  <h1> Connexion </h1>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    </div>
+  </head>
+
+  <body>
+  <form id="login_form" action="connected.php" method="POST">
+    <table>
+        <tr>
+            <th>Login :</th>
+            <td><input type="text" name="login"></td>
+        </tr>
+        <tr>
+            <th>Mot de passe :</th>
+            <td><input type="password" name="password"></td>
+        </tr>
+        <tr>
+            <th></th>
+            <td><input type="submit" value="Se connecter..." /></td>
+            
+        </tr>
+    </table>
+</form>
+
+<a href="inscription.php" class="btn btn-outline-dark" tabindex="-1" role="button" aria-disabled="true">M'inscrire</a>
+</body>
+
+
+
+
+  
+  </html>
---
+++ Projet/inscription.php
@@ -0,0 +1,37 @@
+<form id="inscription" action="validation.php" method="POST">
+    <table>
+    <tr>
+<th>Login :</th>
+<td><input type="text" name="NewLogin"></td>
+    </tr>
+    <tr>
+<th>Pseudo :</th>
+<td><input type="text" name="NewPseudo"></td>
+    </tr>
+    <tr>
+<th>Mot de passe :</th>
+<td><input type="password" name="NewPassword"></td>
+    </tr>
+    <tr>
+<th>Confirmation mot de passe :</th>
+<td><input type="password" name="NewPassword"></td>
+    </tr>
+    <tr>
+<th>Sexe : </th>
+<td><input type="sexe" name="Sexe" /></td>
+    </tr>
+    <tr>
+<th>Age :</th>
+<td><input type="age" name="Age" /></td>
+    </tr>
+    <tr>
+<th>Niveau de pratique : </th>
+<td><input type="sport" name="Niveau de pratique sportive" /></td>
+    </tr>
+    <tr>
+<th></th>
+<td><input type="submit" value="Inscription" /></td>
+    </tr>
+
+</table>
+</form>
