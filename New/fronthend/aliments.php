c04d58812f9aa2ee950bc607b83cdc86e4df2948
PaulinePich <62943136+PaulinePich@users.noreply.github.com>
2021-04-05 19:27:08 +0200

    nass

--- New/fronthend/aliments.php
+++ New/fronthend/aliments.php
@@ -3,11 +3,15 @@
     <head>
         <meta charset='utf-8'>
         
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
         <script src="js/aliments.js" async></script>
         
         <title>Aliments</title>
         <link rel="stylesheet" href="css/style.css">
     </head>
+
     
+    
+
 </html>
+
+
